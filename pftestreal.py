9437df44462ad443126ba5fecf7da1fbac92fc80
Eric Jonas <jonas@ericjonas.com>
2012-12-31 08:27:40 -0700

    Plots reviel totally unreasonable values for theta, need to fix by making the prior non-shit

--- pftestreal.py
+++ pftestreal.py
@@ -22,10 +22,10 @@ PIX_THRESHOLD = 200
 FL_DATA = "data/fl"
 def params():
     PARTICLEN = 1000
-    FRAMEN = 5000
+    FRAMEN = 500
     EPOCHS = ['bukowski_04.W2']
     epoch = EPOCHS[0]
-    for posnoise in [0.01]:
+    for posnoise in [0.001, 0.005, ]:
         for velnoise in [0.001, 0.01]:
             
             infile = [os.path.join(FL_DATA, epoch), 
@@ -94,12 +94,13 @@ def pf_run((epoch_dir, epoch_config_filename,
 def params_rendered():
     for p in params():
          yield ((p[0][0], p[0][1], p[1]), (p[1] + ".png", 
-                                           p[1] + ".xy.png"))
+                                           p[1] + ".xy.png", 
+                                           p[1] + ".examples.png"))
 
 @follows(pf_run)
 @files(params_rendered)
 def pf_plot((epoch_dir, epoch_config_filename, particles_file), 
-            (all_plot_filename, just_xy_filename)):
+            (all_plot_filename, just_xy_filename, examples_filename)):
     
     T_DELTA = 1/30.
     
@@ -109,9 +110,19 @@ def pf_plot((epoch_dir, epoch_config_filename, particles_file),
     particles = a['particles'][:FRAMES]
     N = len(particles)
 
+    led_params_filename = os.path.join(epoch_dir, "led.params.pickle")
+
     cf = pickle.load(open(epoch_config_filename))
     truth = np.load(os.path.join(epoch_dir, 'positions.npy'))
-    
+    env = util.Environmentz(cf['field_dim_m'], 
+                            cf['frame_dim_pix'])
+    led_params = pickle.load(open(led_params_filename, 'r'))
+
+    eoparams = measure.led_params_to_EO(cf, led_params)
+
+    eo = likelihood.EvaluateObj(*cf['frame_dim_pix'])
+    eo.set_params(*eoparams)
+
     STATEVARS = ['x', 'y', 'xdot', 'ydot', 'phi', 'theta']
     # convert types
     vals = dict([(x, []) for x in STATEVARS])
@@ -121,9 +132,13 @@ def pf_plot((epoch_dir, epoch_config_filename, particles_file),
     for v in STATEVARS:
         vals[v] = np.array(vals[v])
 
+    vals_dict = {}
+
     pylab.figure(figsize=(8, 10))
     for vi, v in enumerate(STATEVARS):
         v_bar = np.average(vals[v], axis=1, weights=weights)
+        vals_dict[v] = v_bar
+
         x = np.arange(0, len(v_bar))
         cred = np.zeros((len(x), 2), dtype=np.float)
         for ci, (p, w) in enumerate(zip(vals[v], weights)):
@@ -156,13 +171,14 @@ def pf_plot((epoch_dir, epoch_config_filename, particles_file),
     pylab.savefig(all_plot_filename, dpi=400)
 
     f2 = pylab.figure(figsize=(16, 8))
+
+
     for vi, v in enumerate(['x', 'y']):
         v_bar = np.average(vals[v], axis=1, weights=weights)
         x = np.arange(0, len(v_bar))
         cred = np.zeros((len(x), 2), dtype=np.float)
         for ci, (p, w) in enumerate(zip(vals[v], weights)):
             cred[ci] = util.credible_interval(p, w)
-
         ax = pylab.subplot(2, 1, vi+1)
 
         ax.plot(x, v_bar, color='b')
@@ -172,13 +188,83 @@ def pf_plot((epoch_dir, epoch_config_filename, particles_file),
         ax.scatter(np.arange(N), truth[v][:N], 
                    linewidth=0, s=1, c='k')
         for i in np.argwhere(np.isnan(truth[v][:N])):
-            ax.axvline(i, c='r')
+            ax.axvline(i, c='r', linewidth=0.1, alpha=0.4)
 
         ax.grid(1)
         ax.set_xlim((0, N))
         
     pylab.savefig(just_xy_filename, dpi=300)
                          
-                       
-    
+    # find the errors per point
+
+    PLOT_ERRORS = 12
+
+    errors = np.zeros(len(vals), dtype=np.float32)
+    deltas = np.sqrt((vals_dict['x'] - truth['x'][:N])**2  + \
+                    (vals_dict['y'] - truth['y'][:N])**2)    
+    deltas[np.isnan(deltas)] = -1 # only to find the Nans and make sorting work
+
+    # find index of errors in decreasing order
+    error_i = np.argsort(deltas)[::-1]
+    errs = error_i[:PLOT_ERRORS]
+    error_frames = organizedata.get_frames(epoch_dir, 
+                                           errs)
+
+    f = pylab.figure()
+    WINDOW_PIX = 30
+    for plot_error_i, error_frame_i in enumerate(errs):
+        ax = pylab.subplot(3, 4, plot_error_i + 1)
+        ax.imshow(error_frames[plot_error_i], 
+                  interpolation='nearest', cmap=pylab.cm.gray)
+        true_x = truth['x'][errs[plot_error_i]]
+        true_y = truth['y'][errs[plot_error_i]]
+        true_x_pix, true_y_pix = env.gc.real_to_image(true_x, true_y)
+        print true_x_pix, true_y_pix
+
+        est_x  = vals_dict['x'][error_frame_i]
+        est_y = vals_dict['y'][error_frame_i]
+        est_phi = vals_dict['phi'][error_frame_i]
+        est_theta = vals_dict['theta'][error_frame_i]
+
+        est_x_pix, est_y_pix = env.gc.real_to_image(est_x, est_y)
+        
+
+        rendered_img = eo.render_source(est_x_pix, est_y_pix, 
+                                        est_phi, est_theta)
+
+            # now compute position of diodes
+        front_pos, back_pos = util.compute_pos(eo.length, est_x_pix, 
+                                               est_y_pix, 
+                                               est_phi, est_theta)
+
+        cir = pylab.Circle(front_pos, radius=eoparams[1],  
+                           ec='g', fill=False,
+                           linewidth=2)
+        ax.add_patch(cir)
+        cir = pylab.Circle(back_pos, radius=eoparams[2],  
+                           ec='r', fill=False, 
+                           linewidth=2)
+        ax.add_patch(cir)
+
+        # true
+        ax.axhline(true_y_pix, c='b')
+        ax.axvline(true_x_pix, c='b')
+        # extimated
+        ax.axhline(est_y_pix, c='y')
+        ax.axvline(est_x_pix, c='y')
+
+        # LED points in ground truth
+        for field_name, color in [('led_front', 'g'), ('led_back', 'r')]:
+            lpx, lpy = env.gc.real_to_image(*truth[field_name][error_frame_i])
+            ax.scatter([lpx], [lpy], c=color)
+
+
+        ax.set_xlim((true_x_pix - WINDOW_PIX, true_x_pix + WINDOW_PIX))
+        ax.set_ylim((true_y_pix - WINDOW_PIX, true_y_pix + WINDOW_PIX))
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.set_title(error_frame_i)
+
+    pylab.savefig(examples_filename, dpi=300)
+
 pipeline_run([pf_run, pf_plot], multiprocess=6)
