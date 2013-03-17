3aa8ffff62100589b4e2ab3c8e86454e876c4b09
Eric Jonas <jonas@ericjonas.com>
2013-03-16 18:44:40 -0700

    deterministic analysis plotting for func2 looks great; good results for a lot of datasets.

--- deterministic_analysis.py
+++ deterministic_analysis.py
@@ -82,26 +82,27 @@ def det_run((epoch_dir, epoch_config_filename,
     if frame_end > cf['end_f']:
         frame_end = cf['end_f']
 
+    truth = np.load(os.path.join(epoch_dir, 'positions.npy'))
+    truth_interp, missing = measure.interpolate(truth)
+    derived_truth = measure.compute_derived(truth_interp, 
+                                            T_DELTA)
+
+
     frame_pos = np.arange(frame_start, frame_end)
     # load frames
     frames = organizedata.get_frames(epoch_dir, frame_pos)
 
     FRAMEN = len(frames)
-    THOLDS = [100, 200, 220, 240]
 
-    tholds = {thold : np.zeros(FRAMEN, dtype=np.uint32) for thold in THOLDS}
     coordinates = []
     
     regions = np.zeros((FRAMEN, frames[0].shape[0], frames[0].shape[1]), 
                        dtype=np.uint8)
-    point_est_track_data = np.zeros(FRAMEN, dtype=model.DTYPE_LATENT_STATE)
+    point_est_track_data = []
 
     for fi, frame in enumerate(frames):
         print fi, frame_pos[fi]
         abs_frame_index = frame_pos[fi]
-
-        for thold in THOLDS:
-            tholds[thold][fi] = np.sum(frame > thold)
 
         coordinates.append(skimage.feature.peak_local_max(frame, 
                                                           min_distance=30, 
@@ -109,14 +110,19 @@ def det_run((epoch_dir, epoch_config_filename,
 
         frame_regions = filters.label_regions(frame, mark_min=120, mark_max=220)
         regions[fi] = frame_regions
-        point_est_track_data[fi] = dettrack.point_est_track2(frame, env, eo_params)
+        point_est_track_data.append(dettrack.point_est_track2(frame, env, eo_params))
 
     pickle.dump({'frame_pos' : frame_pos, 
-                 'tholds' : tholds, 
                  'coordinates' : coordinates, 
                  'regions' : regions, 
                  'eo_params' : eo_params, 
-                 'point_est_track' : point_est_track_data}, 
+                 'point_est_track' : point_est_track_data, 
+                 'truth' : truth, 
+                 'missing' : missing, 
+                 'truth_interp' : truth_interp, 
+                 'derived_truth' : derived_truth, 
+                 'epoch' : epoch, 
+                 'frame_start' : frame_start}, 
                 open(outfile, 'w'))
 
 
@@ -154,7 +160,7 @@ def det_plot((epoch_dir, epoch_config_filename, results),
     ax_frames = f1.add_subplot(ROWN, 1, 1)
     ax_truth = f1.add_subplot(ROWN, 1, 2)
     ax_coord_cnt = f1.add_subplot(ROWN, 1, 3)
-    ax_coord_centroid = f1.add_subplot(ROWN, 1, 4)
+    ax_phi_theta = f1.add_subplot(ROWN, 1, 4)
     ax_coord_filt_mean = f1.add_subplot(ROWN, 1, 5)
     ax_img_pos = f1.add_subplot(ROWN, 1, 6)
 
@@ -193,18 +199,29 @@ def det_plot((epoch_dir, epoch_config_filename, results),
         if len(rp) > 0 :
             centroid_row, centroid_col = rp[0]['Centroid']
             centroids[rpi] = env.gc.image_to_real(centroid_col, centroid_row)
-        
-    ax_coord_centroid.plot(frame_pos, truth_interp[frame_pos]['x'], c='b')
-    ax_coord_centroid.plot(frame_pos, truth_interp[frame_pos]['y'], c='r')
-    ax_coord_centroid.scatter(frame_pos, centroids[:, 0], c='b', linewidth=0, s=4)
-    ax_coord_centroid.scatter(frame_pos, centroids[:, 1], c='r', linewidth=0, s=4)
-    ax_coord_centroid.set_xlim(np.min(frame_pos), np.max(frame_pos))
+    ax_phi_theta.plot(frame_pos, derived_truth['phi'][frame_pos])
+    # ax_coord_centroid.plot(frame_pos, truth_interp[frame_pos]['x'], c='b')
+    # ax_coord_centroid.plot(frame_pos, truth_interp[frame_pos]['y'], c='r')
+    # ax_coord_centroid.scatter(frame_pos, centroids[:, 0], c='b', linewidth=0, s=4)
+    # ax_coord_centroid.scatter(frame_pos, centroids[:, 1], c='r', linewidth=0, s=4)
+    # ax_coord_centroid.set_xlim(np.min(frame_pos), np.max(frame_pos))
     
     for ax, coord in [ (ax_coord_filt_mean, point_est_track)]:
         ax.plot(frame_pos, truth_interp[frame_pos]['x'], c='b')
         ax.plot(frame_pos, truth_interp[frame_pos]['y'], c='r')
-        ax.scatter(frame_pos, coord['x'], c='b', linewidth=0, s=4)
-        ax.scatter(frame_pos, coord['y'], c='r', linewidth=0, s=4)
+        have_est = np.array([p.shape[0] > 0 for p in coord])
+        frame_pos_pres = frame_pos[have_est]
+        frame_pos_abs = frame_pos[np.logical_not(have_est)]
+
+        coord_x = [coord[i][0]['x'] for i in np.argwhere(have_est)]
+        coord_y = [coord[i][0]['y'] for i in np.argwhere(have_est)]
+
+        ax.scatter(frame_pos_pres, coord_x, c='b', linewidth=0, s=4)
+        ax.scatter(frame_pos_pres, coord_y, c='r', linewidth=0, s=4)
+
+        for f in frame_pos_abs:
+            ax.axvline(f, c='k', alpha=0.5)
+
         ax.set_xlim(np.min(frame_pos), np.max(frame_pos))
         
                                  
@@ -231,4 +248,81 @@ def det_plot((epoch_dir, epoch_config_filename, results),
     pylab.savefig(all_plot_filename, dpi=300)
 
 
-pipeline_run([det_run, det_plot], multiprocess=3)
+@merge(det_run, 'deterministic.results.pickle')
+def aggregate_results(det_run, outfilename):
+    summary = {}
+    for infile in det_run:
+        print "opening", infile
+        d = pickle.load(open(infile))
+        epoch = d['epoch']
+        point_est_track = d['point_est_track']
+        frame_pos = d['frame_pos']
+
+        have_est = np.array([p.shape[0] > 0 for p in point_est_track])
+        frame_pos_pres = frame_pos[have_est]
+        frame_pos_abs = frame_pos[np.logical_not(have_est)]
+        
+        frame_idx_pres = np.argwhere(have_est).flatten()
+        print frame_idx_pres
+        coord_x = np.array([point_est_track[i][0]['x'] for i in frame_idx_pres])
+        true_x = d['truth_interp'][frame_pos_pres]['x']
+        
+        coord_y = np.array([point_est_track[i][0]['y'] for i in frame_idx_pres])
+        true_y = d['truth_interp'][frame_pos_pres]['y']
+        
+        error_xy = np.sqrt((coord_x-true_x)**2 + (coord_y-true_y)**2)
+        # print error_xy.shape, error_xy.dtype, np.mean(error_xy), np.max(error_xy)
+        # pylab.subplot(3, 1, 1)
+        # pylab.plot(frame_pos_pres, error_xy)
+        # pylab.subplot(3, 1, 2)
+        # pylab.plot(frame_pos_pres, coord_x)
+        # pylab.plot(frame_pos_pres, true_x)
+        # pylab.subplot(3, 1, 3)
+        # pylab.plot(frame_pos_pres, coord_y)
+        # pylab.plot(frame_pos_pres, true_y)
+        
+        # pylab.show()
+        error_xy_mean = np.mean(error_xy)
+        error_xy_median = np.median(error_xy)
+        if len(error_xy) > 0:
+            error_xy_max = np.max(error_xy)
+        else:
+            error_xy_max = 0.0
+        
+        summary["%s.%d" % (epoch,  frame_pos[0])] = {'frame_pos_pres' : len(frame_pos_pres), 
+                                                     'frame_n' : len(frame_pos), 
+                                                     'error_xy_mean' : error_xy_mean, 
+                                                     'error_xy_median' : error_xy_median, 
+                                                     'error_xy_max' : error_xy_max}
+        
+    pickle.dump(summary, open(outfilename, 'w'))
+
+@files(aggregate_results, ('deterministic.fractions.png',
+                           'deterministic.frac_vs_err.png'))
+def plot_agg(infile, (fractions_filename, frac_vs_err_filename)):
+    aggdata = pickle.load(open(infile, 'r'))
+    
+    ## fractions
+    fracs = []
+    per_xy_errors = []
+    for epoch, data in aggdata.iteritems():
+        frac = float(data['frame_pos_pres']) / data['frame_n']
+        fracs.append(frac)
+        per_xy_errors.append(data['error_xy_mean'])
+        print epoch, data['error_xy_mean'], frac
+    fracs = np.array(fracs)
+    per_xy_errors = np.array(per_xy_errors)
+
+    fracs_sorted = np.sort(fracs)[::-1]
+    pylab.plot(fracs_sorted)
+    pylab.savefig(fractions_filename)
+
+    fig = pylab.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    ax.scatter(fracs, per_xy_errors)
+    ax.set_yscale('log')
+    pylab.savefig(frac_vs_err_filename)
+
+pipeline_run([det_run, det_plot, 
+              aggregate_results, 
+              plot_agg], multiprocess=3)
--- dettrack.py
+++ dettrack.py
@@ -53,21 +53,21 @@ def point_est_track(img, env, eo_params):
                      0, 0, 0, 0), dtype=model.DTYPE_LATENT_STATE)
 
 
-def find_possible_front_diodes(img, eo_params):
+def find_possible_front_diodes(img, eo_params, im_reg):
     DIODE_SEP = eo_params[0]
     FRONT_SIZE = float(eo_params[1])
     BACK_SIZE = float(eo_params[2])
-    print "FRONT_SIZE=", FRONT_SIZE, "BACK_SIZE=", BACK_SIZE
     size_thold = (FRONT_SIZE+BACK_SIZE) * 2.5
 
 
-    im_reg = filters.extract_region_filter(img, size_thold=size_thold, 
-                                           mark_min=100, mark_max=210)
-
-    #im_rf = (im_reg>0).astype(float)*255
-    im_rf = img.copy()
+    im_rf = (im_reg>0).astype(float)*255
+    #im_rf = img.copy()
     im_rf[im_reg == 0] = 0
     im_f = ndimage.gaussian_filter(im_rf, FRONT_SIZE)
+    coordinates = skimage.feature.peak_local_max(im_f, 
+                                                 min_distance=FRONT_SIZE+BACK_SIZE, 
+                                                 threshold_rel=0.7)
+
     # pylab.subplot(1, 3, 1)
     # pylab.imshow(img, interpolation='nearest', cmap=pylab.cm.gray)
     # pylab.subplot(1, 3, 2)
@@ -75,15 +75,14 @@ def find_possible_front_diodes(img, eo_params):
     # pylab.subplot(1, 3, 3)
     # pylab.imshow(im_f, interpolation='nearest', cmap=pylab.cm.gray)
     # pylab.show()    
-    coordinates = skimage.feature.peak_local_max(im_f, 
-                                                 min_distance=FRONT_SIZE+BACK_SIZE, 
-                                                 threshold_rel=0.7)
+
     return coordinates
 
 def dist(p1, p2):
     return np.sqrt(np.sum((p1 - p2)**2))
 
-def find_possible_back_diodes(img, eo_params, candidate_front_diodes):
+def find_possible_back_diodes(img, eo_params, candidate_front_diodes, 
+                              im_reg):
     """
     For each candidate front diode returns the list of possible back diodes
     """
@@ -93,10 +92,6 @@ def find_possible_back_diodes(img, eo_params, candidate_front_diodes):
     BACK_SIZE = float(eo_params[2])
 
     size_thold = (FRONT_SIZE+BACK_SIZE) * 2.5
-
-
-    im_reg = filters.extract_region_filter(img, size_thold=size_thold, 
-                                           mark_min=100, mark_max=220)
 
     # this is fun, region properties must be increasing
     
@@ -109,24 +104,43 @@ def find_possible_back_diodes(img, eo_params, candidate_front_diodes):
         for p in props:
             centroid = p['Centroid']
             d = dist(centroid, c)
+            # right now this removes the regions that are too close; 
+            # other ideas include erosion or just removing all pix that are too close
+            # I don't care about the centroid, I care about whether or not it's the same thing
             if d <= FRONT_SIZE:
                 im_reg_c[im_reg == p['Label']] = 0
 
         im_rf = (im_reg_c>0).astype(float)*255
     
         im_f = ndimage.gaussian_filter(im_rf, BACK_SIZE)
-    
+        
         coordinates = skimage.feature.peak_local_max(im_f, 
                                                      min_distance=(FRONT_SIZE+BACK_SIZE), 
                                                      threshold_rel=0.7)
+        
+        # ax = pylab.subplot(1, 3, 1)
+        # pylab.imshow(img, interpolation='nearest', cmap=pylab.cm.gray)
+        # ax = pylab.subplot(1, 3, 2)
+        # ax.imshow(im_reg_c, interpolation='nearest')
+        # circ = pylab.Circle((c[1], c[0]), radius=FRONT_SIZE, 
+        #                     color='g')
+        # ax.add_patch(circ)
+        # ax.plot([c[1]], [c[0]], 'r.')
+
+        # pylab.subplot(1, 3, 3)
+        # pylab.imshow(im_f, interpolation='nearest', cmap=pylab.cm.gray)
+        # pylab.show()    
+
         out_coords.append(coordinates)
     return out_coords
     
 def filter_plausible_points(front, back_list, max_dist):
     ret = []
     for b in back_list:
-        if dist(front, b) < max_dist:
+        if dist(front, b) <= max_dist:
             ret.append(b)
+        else:
+            print "dist", dist(front, b), "is >= max_dist", max_dist
 
     return ret
 
@@ -135,35 +149,57 @@ def point_est_track2(img, env, eo_params):
     1. get regions / filter for things that are interesting
     2. 
 
+    Note that we return "candidate points", and if we don't know we return 0
     """
     
     # min_distance is the dist between pix for peaks. Not sure if this
     # should be a min or a max for the thing
 
     # size_thold = the largest-sized region we should allow
+    DIODE_SEP = eo_params[0]
+    FRONT_SIZE = float(eo_params[1])
+    BACK_SIZE = float(eo_params[2])
+    size_thold = (FRONT_SIZE+BACK_SIZE) * 2.5
 
-    size_thold = 2*(eo_params[1] + eo_params[2]) * 1.2
-    min_distance = eo_params[0]
+    im_reg_coarse = filters.extract_region_filter(img, size_thold=size_thold, 
+                                                  mark_min=100, mark_max=230)
 
-    coord_means = [0, 0]
+    im_reg_fine = filters.extract_region_filter(img, size_thold=size_thold, 
+                                                mark_min=220, mark_max=240)
+    im_reg_fine[im_reg_coarse ==0] = 0
+    # pylab.imshow(im_reg_fine)
+    # pylab.show()
+
+    min_distance = DIODE_SEP
+
     
-    front_c = find_possible_front_diodes(img, eo_params)
+    front_c = find_possible_front_diodes(img, eo_params, im_reg_fine)
+
+    def none():
+        return np.zeros(0, dtype=model.DTYPE_LATENT_STATE)
+
 
     if len(front_c) > 0:
-        back_c = find_possible_back_diodes(img, eo_params, front_c)
-
+        back_c = find_possible_back_diodes(img, eo_params, front_c, im_reg_fine)
+        print "possible back diodes", back_c
         # just take the first one, because TEST
         plaus_back = filter_plausible_points(front_c[0], back_c[0], 
-                                             eo_params[0]*1.5)
+                                             eo_params[0]*2.0)
+        print "plausible back diodes", plaus_back
+
         if len(plaus_back) > 0:
             a = np.vstack([front_c[0], plaus_back[0]])
 
             coord_means = env.gc.image_to_real(*np.mean(np.fliplr(a),
                                                         axis=0))
+        else:
+            return none()
+    else:
+        return none()
 
-    return np.array((coord_means[0], 
+    return np.array([(coord_means[0], 
                      coord_means[1], 
-                     0, 0, 0, 0), dtype=model.DTYPE_LATENT_STATE)
+                     0, 0, 0, 0)], dtype=model.DTYPE_LATENT_STATE)
 
 
 
